7e7dbdd40af4d073abcb7ed63599e88e41ffd78e
sheggz <emmanuelikoya99@gmail.com>
2022-06-28 03:42:32 +0000

    implement string concatenation with f-strings

---
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+str = f"{str[39:66]} {str[-22:-18]} {str[0:7]}"
+print(str)
