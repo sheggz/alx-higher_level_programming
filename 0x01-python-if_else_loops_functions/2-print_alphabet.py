9386a15e2fd8dceb6cb9da6412b30db31f71ce31
sheggz <emmanuelikoya99@gmail.com>
2022-06-28 22:08:28 +0000

    [Create] python script to print lower case ASCII letters

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 for x in range(97, 123):
-    print(chr(x), end='')
+    print(f'{x:c}', end='')
 # chr(i) returns the string representing a character whose ascii value is i
 # end = '' changes the default '\n' which ends the print statement to nothing
