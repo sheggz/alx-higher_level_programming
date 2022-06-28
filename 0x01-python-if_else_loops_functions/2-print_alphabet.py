9dfbf4e9f7ecdc6e60d34209830738120c9c741b
sheggz <emmanuelikoya99@gmail.com>
2022-06-28 22:15:13 +0000

    [fix] substitute f-string for str.format()

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 for x in range(97, 123):
-    print(f'{x:c}', end='')
+    print("{:c}".format(x), end='')
 # chr(i) returns the string representing a character whose ascii value is i
 # end = '' changes the default '\n' which ends the print statement to nothing
