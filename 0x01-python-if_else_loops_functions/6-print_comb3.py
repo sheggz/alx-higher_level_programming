7f2a8aaa17d5f948cc704c59a4c1647676449be7
sheggz <emmanuelikoya99@gmail.com>
2022-06-29 00:53:56 +0000

    [Create] python script to print combo of 2 diff digits

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+for i in range(10):
+    for j in range(10):
+        # since we're only to print the smallest combination
+        if i < j:
+            if (i == 8 and j == 9):
+                print("{:d}{:d}".format(i,j))
+            else: 
+                print("{:d}{:d}, ".format(i,j), end='')
