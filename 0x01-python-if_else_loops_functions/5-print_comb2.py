10d124822e32c2d9db134595596299baa909cf5f
sheggz <emmanuelikoya99@gmail.com>
2022-06-29 00:32:26 +0000

    [Create] python script that prints 0-99 with a width of 2

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -3,5 +3,7 @@ for x in range(100):
     if x < 99:
         print("{:02}, ".format(x), end="")
     elif x == 99:
-        print("{:02d}".format(x))  ''' i'm not changing the end since
-        last mumber should be follwed by new line'''
+        print("{:02d}".format(x))
+        # i'm not changing the end since
+        # last mumber should be follwed by new line
+        
