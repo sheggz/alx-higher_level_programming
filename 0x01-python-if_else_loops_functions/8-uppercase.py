6c7fa4bda25c7546ce4ee88143da608eb3f605c4
sheggz <emmanuelikoya99@gmail.com>
2022-06-29 01:52:20 +0000

    [create] python fn to convert lower case string to upper

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -2,4 +2,4 @@
 def uppercase(str):
     for char in str:
         if ord(char) >= ord('a') and ord(char) <= ord('z'):
-            print("{:c}".format(ord(char) + (ord('a') - ord('A'))), end='')
+            print("{:c}".format(ord(char) + (ord('A') - ord('a'))), end='')
