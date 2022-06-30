45456c66ec2c6a617632bd3608e7be4a8bdd89db
sheggz <emmanuelikoya99@gmail.com>
2022-06-30 20:56:59 +0000

    [Create] python script to add all i/p cmd line args

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 import sys
 sum = 0
-for x in range (1, len(sys.argv)):
+for x in range(1, len(sys.argv)):
     sum = sum + int(sys.argv[x])
 if __name__ == '__main__':
     print(sum)
-    
-
