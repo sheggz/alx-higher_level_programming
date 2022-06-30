636af50c9cc861025e42511a74498b6c02afaddf
sheggz <emmanuelikoya99@gmail.com>
2022-06-30 21:16:19 +0000

    Create python script to calculate basic opeations

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from calculator_1 import sub, add, mul, div
+    import sys
+    if len(sys.argv) != 4:
+        print('Usage: ./100-my_calculator.py <a> <operator> <b>')
+        sys.exit(1)
+    a = int(sys.argv[1])
+    op = sys.argv[2]
+    b = int(sys.argv[3])
+    if op == '+':
+        result = add(a, b)
+    elif op == '-':
+        result = sub(a, b)
+    elif op == '*':
+        result = mul(a, b)
+    elif op == '/':
+        result = div(a, b)
+    else:
+        print('Unknown operator. Available operators: +, -, * and /')
+        sys.exit(1)
+    print(f'{a} {op} {b} = {result}')
