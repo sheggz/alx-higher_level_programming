9a86d14bdb464ce195b23871ffb5402a60aae6b2
sheggz <emmanuelikoya99@gmail.com>
2022-06-30 20:39:05 +0000

    [Fix] revert to old if-else style

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,8 +2,14 @@
 if __name__ == '__main__':
     import sys
     count = len(sys.argv) - 1
-    plural = 's' if count != 1 else plural = ''
-    sign = ':' if count > 0 else sign = '.'
+    if count != 1:
+        plural = 's'
+    else:
+        plural = ''
+    if count > 0:
+        sign = ':'
+    else:
+        sign = '.'
     print('{} argument{}{}'.format(count, plural, sign))
     for i in range(1, count + 1):
-        print('{}: {}'.format(i, sys.argv[i]'))
+        print('{}: {}'.format(i, sys.argv[i]))
