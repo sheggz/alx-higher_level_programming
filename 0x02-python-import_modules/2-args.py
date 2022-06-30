dc8b24ea6ab28ce53074adb3951a4d10d0921f06
sheggz <emmanuelikoya99@gmail.com>
2022-06-30 20:32:23 +0000

    [Create] py script to print cmd line args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,8 +2,8 @@
 if __name__ == '__main__':
     import sys
     count = len(sys.argv) - 1
-    plural = 's' if count != 1 else ''
-    sign = ':' if count > 0 else '.'
+    plural = 's' if count != 1 else plural = ''
+    sign = ':' if count > 0 else sign = '.'
     print('{} argument{}{}'.format(count, plural, sign))
     for i in range(1, count + 1):
-        print('{}: {}'.format(i, sys.argv[i]))
+        print('{}: {}'.format(i, sys.argv[i]'))
