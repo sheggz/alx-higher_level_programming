11a4218a5f6b5a1621d86616b15cb345b03b10d6
sheggz <emmanuelikoya99@gmail.com>
2022-07-05 01:56:56 +0000

    [update] print using str.format()

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -6,8 +6,8 @@ def print_matrix_integer(matrix=[[]]):
         for i in range(row):
             for j in range(column):
                 if j == column - 1:
-                    print(matrix[i][j])
+                    print("{:d}".format(matrix[i][j]))
                 else:
-                    print(matrix[i][j], end=" ")
+                    print("{:d}".format(matrix[i][j], end=" "))
     else:
-        print()
+        print("{}".format())
