1829de1eefee4ffd4702be0427614cd7b0e48669
sheggz <emmanuelikoya99@gmail.com>
2022-07-06 22:42:53 +0000

    [Create] a py func to replace a parameter in a given list

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# a func that replaces all occurrences of an element by another in a new list.
+
+def search_replace(my_list, search, replace):
+    localcpy = my_list.copy()
+    # i'm using a loop because list.index() will only return the index of 1st 
+    # occurence of the search parameter
+    for x in localcpy:
+        if x == search:
+            localcpy[localcpy.index(search)] = replace
+    return localcpy
