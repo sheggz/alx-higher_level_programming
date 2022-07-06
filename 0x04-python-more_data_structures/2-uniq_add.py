210688ef3656a1f60a0ca59b64cee9fccda79a8b
sheggz <emmanuelikoya99@gmail.com>
2022-07-06 23:05:44 +0000

    [Create] py function to add unique elements of a list

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 
-#  a function that adds all unique integers in a list (only once for each integer)
+#  a func that adds all unique integers in a list (only once for each integer)
 
 def uniq_add(my_list=[]):
     # since we are working with unique entries we have to create a set
-    sum = 0;
+    sum = 0
     for x in set(my_list):
         sum += x
     return sum
