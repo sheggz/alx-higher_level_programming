ae8daeca60c4c265552e40293f72418b13508c94
sheggz <emmanuelikoya99@gmail.com>
2022-07-07 02:57:57 +0000

    [Craete] py func that leverages maps as againt loops

---
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+# a function that returns a list with all values multiplied by a number
+# without using any loops.
+def multiply_list_map(my_list=[], number=0):
+    return list(map(lambda x: x * number, my_list))
