beabfeddf76572ff59a6ecb00225ece3bdfb2734
sheggz <emmanuelikoya99@gmail.com>
2022-07-20 05:01:41 +0000

    use setter and getter

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""This module implents some of the Class basics
+
+"""
+
+
+class Square:
+    """A Class to implement a square
+    """
+    def __init__(self, size=0):
+        """Initialize the size attribute
+
+        Args:
+            size (int): the size of the square
+        """
+
+        self.size = size  #: call the setter property fn
+
+    @property
+    def size(self):
+        """Create a getter
+
+        This will be used for accessing the private element
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """A setter function
+        to set the private Variable with conditions
+
+        Args:
+            the size to be set
+        """
+
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value  #: if no error is raised, set private attr
+
+    def area(self):
+        """function to compute area"""
+        return (self.__size **2)
