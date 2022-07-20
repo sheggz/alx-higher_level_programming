778fd7e2642ffd1451df9643d5c2c13b1dcb92c7
sheggz <emmanuelikoya99@gmail.com>
2022-07-20 04:56:17 +0000

    [Upgrade] prevtask with method to compute area

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Square Class
+A Square Class
+"""
+
+
+class Square:
+    """a class to implement a square"""
+    def __init__(self, size=0):
+        """__init__
+        The __init__ method initializes the size value of the square.
+        Attributes:
+            size (:obj:`int`, optional): The size of the square.
+        Raises:
+            TypeError: If `size` type is not `int`.
+            ValueError: If `size` is less than `0`.
+        """
+
+        if type(size) is not int:
+            raise TypeError('size must be an integer')
+
+        if size < 0:
+            raise ValueError('size must be >= 0')
+
+        self.__size = size
+
+    def area(self):
+        """Class method to compure square area"""
+        return (self.__size ** 2)
