d164eb832a108306bf01c3ba17302881035e4ee7
sheggz <emmanuelikoya99@gmail.com>
2022-08-02 00:49:23 +0000

    [Create] py fn to test of class instance

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""test if an object is an instance of a class"""
+
+
+def is_same_class(obj, a_class):
+    """this function tests if an object is an instance of a class"""
+
+
+    return(isinstance(obj, a_class))
