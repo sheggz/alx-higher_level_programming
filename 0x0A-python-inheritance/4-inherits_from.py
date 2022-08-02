55569713ac1844cae5569ba0dc3d847dd15c90eb
sheggz <emmanuelikoya99@gmail.com>
2022-08-02 01:34:49 +0000

    [Create] py fn that tests for the object of a superclass's subclass

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""check if an object is an instance of the subcls
+of specified superclass"""
+
+
+def inherits_from(obj, a_class):
+    """a function that returns True if the object is an instance
+    of a class that inherited (directly or indirectly) from the
+    specified class ; otherwise False"""
+
+    if issubclass(type(obj), a_class):
+        return True
+    else:
+        return False
