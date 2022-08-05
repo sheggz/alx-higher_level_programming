a1be5b21c2674d9eb8924be58ccc07fce6a55aeb
sheggz <emmanuelikoya99@gmail.com>
2022-08-05 17:37:53 +0000

    Completing the mandatories

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -19,7 +19,7 @@ class Rectangle(BaseGeometry):
         """Method that calculates area of rectangle
         """
 
-        return __self.width * __self.height
+        return self.__width * self.__height
 
     def __str__(self):
         """Magic method to return rectangle description
